ccd48b5e10c3a1534368aa70b8d33eae52d6d9fe
Tomas Lazauskas <12182911+tomaslaz@users.noreply.github.com>
2019-04-03 16:40:14 +0100

    debugging

--- sample_gem5_parameters.py
+++ sample_gem5_parameters.py
@@ -72,13 +72,13 @@ def process_sample(params):
 
     
 
-    print(params)
+    result = gem5.main(params)
 
-    try:
-        result = gem5.main(params)
-    except:
-        result = False
-        
+    # try:
+    #     result = gem5.main(params)
+    # except:
+    #     result = False
+
     return result
 
 if __name__ == "__main__":
